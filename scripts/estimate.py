05a34fb7c29f154083ad62e67b331e47d781d774
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-27 11:24:46 +0530

    using autograd to optimize the distance metric learinng

--- scripts/estimate.py
+++ scripts/estimate.py
@@ -3,18 +3,46 @@ import torch
 import numpy as np
 import scipy.io   
 
+
+# Z scoring the matrix 
+def normalize(matrix):
+        mean = torch.mean(matrix.flatten())
+        sigma = torch.std(matrix.flatten())
+        matrix = (matrix - mean)/sigma
+        return matrix 
+
+# Making a non-symmetrix square matrix symmetric 
+def makeSymmetric(matrix):
+    return 0.5*(matrix + matrix.T)
+
+
+    
+
 # Loading the confusion matrix
 
 confusionMatrix = scipy.io.loadmat("../data/brailletouch.mat")
-confusionMatrix = confusionMatrix["A"]
+confusionMatrix = torch.tensor(confusionMatrix["A"].astype(float))
+confusionMatrix = normalize(confusionMatrix)
+confusionMatrix  = makeSymmetric(confusionMatrix)
 
 # Loading the feature vector matrix according to either of the peano curves 
 
 rowFeatureVectors =  scipy.io.loadmat("../data/FVrow.mat")
-rowFeatureVectors = rowFeatureVectors['Xrow']
+rowFeatureVectors = torch.tensor(rowFeatureVectors['Xrow'].astype(float))
 
 # Initializing the distance metric 
-
 M = torch.rand(6,6)
 
-print(M)
+# Setting the number of epochs 
+num_epochs =  100
+M.requires_grad = True
+
+res = torch.mm(torch.mm(rowFeatureVectors.T,M.double()),rowFeatureVectors)
+
+
+for ep in range(num_epochs):
+    
+    
+
+
+ 
